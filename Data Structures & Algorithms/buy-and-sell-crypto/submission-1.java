b7e47f472468d2b4de892d3167816d1b8253472f
Satya Prakash Nayak <105032869+MELL0WED@users.noreply.github.com>
2026-08-22 18:26:57 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProf = 0;
+        int minVal = prices[0];
+        
+        for(int i=1;i<prices.length;i++){
+            minVal = Math.min(minVal, prices[i]);
+            maxProf = Math.max(maxProf, prices[i] - minVal);
+        }
+        return maxProf;
+    }
+}
